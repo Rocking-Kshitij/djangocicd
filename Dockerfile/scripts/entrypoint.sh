89925ef126521c50689037ed6ef450eee95ef326
#flaming-fists <kshitijz1x2@gmail.com>
2024-10-25 05:13:19 +0530

    test18

--- Dockerfile/scripts/entrypoint.sh
+++ Dockerfile/scripts/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
+echo "executing script"
+dir
+sleep 2
 sed -i "s/db_HOST = localhost/db_HOST = db/" .env
 sed -i "s/db_PORT = 5431/db_PORT = 5432/g" .env
-cd tooplat
 sleep 10
-python manage.py runserver 0.0.0.0:8001
+python ./tooplat/manage.py runserver 0.0.0.0:8001
 
 echo "yee! server started"
