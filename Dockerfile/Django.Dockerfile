eb50da950006cfcdc08caa05c83ed5f513cb22c9
#flaming-fists <kshitijz1x2@gmail.com>
2024-10-25 05:42:22 +0530

    test21

--- Dockerfile/Django.Dockerfile
+++ Dockerfile/Django.Dockerfile
@@ -3,7 +3,6 @@ WORKDIR /app
 COPY requirement.txt /app/
 RUN pip3 install -r requirement.txt
 COPY tooplat /app/tooplat/
-COPY .env /app/
 COPY Dockerfile/scripts/entrypoint.sh /app/
 EXPOSE 8002
 RUN chmod +x entrypoint.sh
--- Dockerfile/scripts/entrypoint.sh
+++ Dockerfile/scripts/entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 echo "executing script"
-dir
+ls
 sleep 2
-sed -i "s/localhost/db/" .env
-sed -i "s/5431/5432/g" .env
+touch .env
+cat <<EOL >.env
+SECRET_KEY = django-insecure-_%en8$^p@yuv3@e6b5t_fx-68fk3n1cf7!^a_c#++@kv)l3^97
+db_name = finance_db
+db_user = django_postgreuser
+db_PASSWORD = django_postgre_password
+db_HOST = db
+db_PORT = 5432
+EOL
 sleep 10
 python ./tooplat/manage.py runserver 0.0.0.0:8001
 
