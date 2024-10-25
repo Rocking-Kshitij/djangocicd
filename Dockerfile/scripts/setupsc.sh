f968469c5d0ea0fb35a68ca17811165796628448
#flaming-fists <kshitijz1x2@gmail.com>
2024-10-25 06:32:17 +0530

    test28

--- Dockerfile/scripts/setupsc.sh
+++ Dockerfile/scripts/setupsc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "executing script"
-sed -i "s/db_HOST = localhost/db_HOST = db/" .env
-sed -i "s/db_PORT = 5431/db_PORT = 5432/g" .env
+sed -i "s/db_HOST = localhost/db_HOST = db/" .env*
+sed -i "s/db_PORT = 5431/db_PORT = 5432/g" .env*
 echo "yee! server started"
