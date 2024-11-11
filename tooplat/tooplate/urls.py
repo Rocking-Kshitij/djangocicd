ad24a99e39e5789a5801cea80a5d4e505ab11903
#flaming-fists <kshitijz1x2@gmail.com>
2024-11-11 10:43:44 +0530

    all pages working

--- tooplat/tooplate/urls.py
+++ tooplat/tooplate/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name = "main-view")
+    path("", views.host_path, name = "main-view"),
+    path("index", views.index, name = "main-view"),
+    path("setting", views.SettingPage.as_view(), name = "setting-view"),
+    path("profile", views.profile_view, name = "profile-view"),
+    path("my_wallet", views.wallet_view, name = "wallet-view"),
+    path("help-center", views.HelpPage.as_view(), name = "Help-view"),
 ]
--- tooplat/tooplate/views.py
+++ tooplat/tooplate/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def index(request):
-    print("view worked")
+def host_path(request):
+    return redirect('/index')
+
+def index(request): # function based view
     return render(request, "./index.html")
+
+def wallet_view(request): # function based view
+    return render(request, "./wallet.html")
+
+def profile_view(request): # function based view
+    return render(request, "./profile.html")
+
+class SettingPage(TemplateView): # class based view
+    template_name = "setting.html"
+
+class HelpPage(TemplateView): # class based view
+    template_name = "help-center.html"
